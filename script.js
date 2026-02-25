f3645310e569e2e3b167d13d1a45a18436a17c18
yashkhanofficial <info.yashkhan@gmail.com>
2026-02-25 06:57:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,3 @@
-// ===== Smooth Scroll =====
-function scrollToSection() {
-  const section = document.getElementById("resources");
-  section.scrollIntoView({ behavior: "smooth" });
-}
-
-// ===== Typing Effect =====
-const text = "Advanced Security • Free Tools • Digital Awareness";
-let index = 0;
-
-function typeEffect() {
-  const typingElement = document.getElementById("typing");
-
-  if (!typingElement) return;
-
-  if (index < text.length) {
-    typingElement.innerHTML += text.charAt(index);
-    index++;
-    setTimeout(typeEffect, 50);
-  }
-}
-
-document.addEventListener("DOMContentLoaded", typeEffect);
-
-// ===== Counter Animation =====
-const counters = document.querySelectorAll(".counter");
-
-counters.forEach(counter => {
-  counter.innerText = "0";
-
-  const updateCounter = () => {
-    const target = +counter.getAttribute("data-target");
-    const current = +counter.innerText;
-
-    const increment = target / 200;
-
-    if (current < target) {
-      counter.innerText = Math.ceil(current + increment);
-      setTimeout(updateCounter, 15);
-    } else {
-      counter.innerText = target;
-    }
-  };
-
-  updateCounter();
-});
-
-
-
 /* Project: Shadow Intel v3
    Status: Stealth Mode Active
 */
@@ -112,3 +63,50 @@ counters.forEach(counter => {
     setTimeout(_initScanner, 2000);
 
 })(window);
+
+// ===== Smooth Scroll =====
+function scrollToSection() {
+  const section = document.getElementById("resources");
+  section.scrollIntoView({ behavior: "smooth" });
+}
+
+// ===== Typing Effect =====
+const text = "Advanced Security • Free Tools • Digital Awareness";
+let index = 0;
+
+function typeEffect() {
+  const typingElement = document.getElementById("typing");
+
+  if (!typingElement) return;
+
+  if (index < text.length) {
+    typingElement.innerHTML += text.charAt(index);
+    index++;
+    setTimeout(typeEffect, 50);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", typeEffect);
+
+// ===== Counter Animation =====
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+  counter.innerText = "0";
+
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+    const current = +counter.innerText;
+
+    const increment = target / 200;
+
+    if (current < target) {
+      counter.innerText = Math.ceil(current + increment);
+      setTimeout(updateCounter, 15);
+    } else {
+      counter.innerText = target;
+    }
+  };
+
+  updateCounter();
+});
